2da814fd848925871fad1e07b86ca3582db4365f
Vallejo Collados <jvallejocollados@deloitte.es>
2026-03-01 19:26:07 +0100

    IDF 6.1 fixes

--- src/controllers/wifi.cpp
+++ src/controllers/wifi.cpp
@@ -151,7 +151,7 @@ WIFI::start_sta(void)
   strcpy((char *) wifi_config.sta.password, wifi_pwd.c_str());
 
   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-  ESP_ERROR_CHECK(esp_wifi_set_config((wifi_interface_t)ESP_IF_WIFI_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
   ESP_ERROR_CHECK(esp_wifi_start());
 
   ESP_LOGD(TAG, "start_sta() finished.");
